e6b3743a40e65386dd8915517024768b5040c7fb
Hady Mohammed <hady.m.meawad@gmail.com>
2025-04-25 16:08:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+
+
+interface Cipher {
+    public void performAction();
+}
+
+
+class InMemoryCipherStrategy implements Cipher {
+    public void performAction() {
+// load in byte[] ....
+    }
+}
+
+
+class SwaptToDiskCipher implements Cipher {
+    public void performAction() {
+// swapt partial results to file.
+    }
+}
+
+
+class Encrypt{
+    private Cipher cipher;
+
+    public Encrypt(String filename) {}
+}
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Encrypt encrypt = new Encrypt("txt.txt");
+    }
+}
